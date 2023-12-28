357e2312a1264c3a85c78d46cc2b953530c6c426
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-12-28 15:30:47 -0600

    Add: B1B -  added  logic for displaying the welcome message, javadocs included

--- src/main/java/edu/wgu/d387_sample_code/convertor/DisplayWelcomeMessage.java
+++ src/main/java/edu/wgu/d387_sample_code/convertor/DisplayWelcomeMessage.java
@@ -3,26 +3,41 @@ package edu.wgu.d387_sample_code.convertor;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class WelcomeMessageProvider implements Runnable {
+/**
+ * The {@code DisplayWelcomeMessage} class is responsible for displaying
+ * a welcome message based on a specified locale. It implements the {@code Runnable}
+ * interface, allowing it to be executed in a separate thread.
+ *
+ * @author nick mensah
+ */
+public class DisplayWelcomeMessage implements Runnable {
 
-    public static String[] getWelcomeMessages(){
-        String[] messages = new String[2];
+    private final Locale locale;
 
-        // English
-        Locale enLocale = new Locale("en", "US");
-        ResourceBundle enBundle = ResourceBundle.getBundle("i18n/welcome", enLocale);
-        messages[0] = enBundle.getString("welcome.message");
-
-        // French
-        Locale frLocale = new Locale("fr", "CA");
-        ResourceBundle frBundle = ResourceBundle.getBundle("i18n/welcome", frLocale);
-        messages[1] = frBundle.getString("welcome.message");
-
-        return messages;
+    //Constructor
+    public DisplayWelcomeMessage(Locale locale) {
+        this.locale = locale;
     }
 
-    @Override
-    public void run() {
-        
-    }
-}
+    /**
+     * Gets the welcome message based on the specified locale. The message is
+     * retrieved from the resource bundle named "i18n/welcome". The resource
+     * bundle contains localized messages for different locales. The key used
+     * to retrieve the message is "welcome.message".
+     *
+     * @return The welcome message for the specified locale.
+     * @throws java.util.MissingResourceException If the message is not found for the given locale.
+     */
+            public String getWelcomeMessage () {
+                // ResourceBundle is used to access locale messages from properties files.
+                ResourceBundle resourceBundle = ResourceBundle.getBundle("i18n/welcome", locale);
+
+                // Retrieve the welcome message using the key "welcome.message" from the resource bundle.
+                return resourceBundle.getString("welcome.message");
+            }
+
+            @Override
+            public void run () {
+
+            }
+        }
