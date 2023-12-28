d294e7ed2625c4489f0e04c52ee5e5910a41cc46
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-12-28 15:15:25 -0600

    Add: B1B - Keeping enpoints secure by adding WebMvcConfigurer Bean

--- src/main/java/edu/wgu/d387_sample_code/config/ApiConfig.java
+++ src/main/java/edu/wgu/d387_sample_code/config/ApiConfig.java
@@ -1,13 +1,25 @@
 package edu.wgu.d387_sample_code.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class ApiConfig implements  WebMvcConfigurer{
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
 //Not needing the object member is a change in recent versions of spring.
 //
 //    @Bean
@@ -25,5 +37,5 @@ public class ApiConfig implements  WebMvcConfigurer{
 //        return objectMapper.writerWithDefaultPrettyPrinter();
 //    }
 //
-
+    }
 }
