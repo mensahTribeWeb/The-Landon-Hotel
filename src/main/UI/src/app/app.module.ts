5326eb958210fc7ff9fd7793fb744e1bd63ef3c7
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-12-28 15:55:45 -0600

    Add: B1B Inject Welcome component into app.component

--- src/main/UI/src/app/app.module.ts
+++ src/main/UI/src/app/app.module.ts
@@ -4,23 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
-
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, WelcomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
